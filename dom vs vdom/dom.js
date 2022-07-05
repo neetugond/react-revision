aab49ec6f1554b0dd6605f544e0c8fc88ddf8717
neetugond <93507242+neetugond@users.noreply.github.com>
2022-07-05 21:01:32 +0530

    dom

--- dom vs vdom/dom.js
+++ dom vs vdom/dom.js
@@ -1,3 +1,9 @@
+// vertual dom - vertual dom is the replica of existing dom, original html page copy
+
+// in react for every dom object there is a corresponding vertual dom object
+
+
+
 var newElement = document.createElement("h2");
 newElement.textContent = "hello from real dom";
 document.querySelector('body').appendChild(newElement)
--- state-mgmt-one/src/App.jsx
+++ state-mgmt-one/src/App.jsx
@@ -1,11 +1,37 @@
+import React from 'react'
 import './App.css'
+
+const name = <span>hello span</span>
+
+// without jsx proper way it take 3 parameter element, className and text
+const newElement = React.createElement("h2", { className: 'newElement' }, 'hello h2 text without jsx syntax')
+
+const text = ()=>{
+alert('calling neetu')
+}
+const input = function (e) {
+  // if (e.key === 'Enter') {
+  //   alert('enter button click')
+  // }
+  if (e >= 18) {
+    return<span>You are eligible</span>
+  } else {
+    return <span>You are not eligible</span>
+  }
+  
+}
 
 function App() {
 
   return (
     <div className="App">
-     <h1>hello</h1>
+      <h1>hello from jsx, {name}</h1>
+      {newElement}
+      {/* <input type="text" value="" onKeyPress={input} /> */}
+      <button onClick={text} style={{backgroundColor: 'black', color: 'white'}} >call neetu</button>
+      {input(17)}
     </div>
+   
   )
 }
 
